37b52eb87b13e1c5024af8ff9b37e119fc73b460
Harsh8089 <harshjain3575@gmail.com>
2026-05-02 13:19:58 +0530

    rev port 5173

--- react-express/frontend/vite.config.ts
+++ react-express/frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['http://ec2-18-234-95-174.compute-1.amazonaws.com']
+  }
 })
